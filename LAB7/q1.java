1ccae92b442166944f32452d780cdebde69be1ec
the-darklord <saimihirnath.nayakanti1@gmail.com>
2022-10-10 15:48:57 +0530

    LAB7

--- LAB7/q1.java
+++ LAB7/q1.java
@@ -1,47 +1,4 @@
-package com.course.structure;
-
-class building
-{
-    private int sqfoot;
-    private int floors;
-    void set(int sqfoot,int floors)
-    {
-        this.sqfoot=sqfoot;
-        this.floors=floors;
-    }
-    void get()
-    {
-        System.out.println("Square Foot : "+sqfoot+" Floors : "+floors);
-    }
-}
-class house extends building
-{
-    private int bedrooms;
-    private int bathrooms;
-    void set(int bedrooms,int bathrooms)
-    {
-        this.bedrooms=bedrooms;
-        this.bathrooms=bathrooms;
-    }
-    void get()
-    {
-        System.out.println("Bed Rooms : "+bedrooms+" Bath Rooms : "+bathrooms);
-    }
-}
-class school extends building
-{
-    private int classrooms;
-    private String gradelevel;
-    void set(int classrooms,String gradelevel)
-    {
-        this.classrooms=classrooms;
-        this.gradelevel=gradelevel;
-    }
-    void get()
-    {
-        System.out.println("Class Rooms : "+classrooms+" Grade Level : "+gradelevel);
-    }
-}
+import com.course.structure.*;
 class q1
 {
     public static void main(String[] args)
--- LAB7/q2.java
+++ LAB7/q2.java
@@ -1,45 +1,4 @@
-package myPackages.p1;
-
-class Maximum
-{
-    static int max(int a,int b,int c)
-    {
-        int large = a>b?(a>c?a:c):(b>c?b:c);
-        return large;
-    }
-    static double max(double a,double b,double c)
-    {
-        double large = a>b?(a>c?a:c):(b>c?b:c);
-        return large;
-    }
-    static int max(int[] arr)
-    {
-        int large = arr[0];
-        for(int i=1;i<arr.length;i++)
-        {
-            if(large<arr[i])
-            {
-                large=arr[i];
-            }
-        }
-        return large;
-    }
-    static int max(int[][] arr)
-    {
-        int large = arr[0][0];
-        for(int i=0;i<arr.length;i++)
-        {
-            for(int j=0;j<arr[0].length;j++)
-            {
-                if(large<arr[i][j])
-                {
-                    large=arr[i][j];
-                }
-            }
-        }
-        return large;
-    }
-}
+import myPackages.p1.*;
 class q2
 {
     public static void main(String[] args)
